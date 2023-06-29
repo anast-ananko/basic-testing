5f9d2d020f33fa9d78fc2506d583f3eae106e850
Nastassia Ananka <anast.ananko@gmail.com>
2023-06-30 00:14:18 +0200

    feat: complete snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,33 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1, 2, 3];
+    const expectedLinkedList = {
+      next: {
+        next: {
+          next: {
+            next: null,
+            value: null,
+          },
+          value: 3,
+        },
+        value: 2,
+      },
+      value: 1,
+    };
+
+    const generatedLinkedList = generateLinkedList(values);
+
+    expect(generatedLinkedList).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = [1, 2, 3];
+    const generatedLinkedList = generateLinkedList(values);
+
+    expect(generatedLinkedList).toMatchSnapshot();
   });
 });
